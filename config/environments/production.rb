1e351facdd0d22ce71e461ef9a32b6c3938f0636
wheatberry <wheatberry@rocketmail.com>
2014-10-02 13:53:20 -0400

    correct another environment variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com", 
     port: 587, 
-    domain: Rails.application.secrets.license-plate-game,
+    domain: Rails.application.secrets.domain_name,
     authentication: "plain", 
     enable_starttls_auto: true, 
     user_name: Rails.application.secrets.email_provider_username,
